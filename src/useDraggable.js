eee3c07515c8b5c23940afd44293101516194964
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2021-03-08 09:20:53 +0800

    Update

--- src/useDraggable.js
+++ src/useDraggable.js
@@ -1,93 +1,155 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 
+/**
+ * 判断事件
+ * @param {Event}
+ * @return {Number} 0: TouchEvent, 1: MouseEvent 2: return
+ */
+const decideEvent = params => {
+  if (window.TouchEvent && params instanceof window.TouchEvent) return 0;
+  if (window.MouseEvent && params instanceof window.MouseEvent && params.button === 0) return 1;
+  return 2;
+};
+
+/**
+ * 拖拽
+ * @returns {Object} bindRef 绑定Dom
+ *                   info 初始位置 当前位置 当前状态
+ */
 const useDraggable = () => {
-  const ref = useRef(null);
+  const bindRef = useRef(null); // 绑定Dom
+  const prevPosition = useRef([0, 0]); // 之前位置
   const [state, setState] = useState({
-    position: [0, 0],
-    dragging: false,
-    prevPosition: [0, 0],
-    startXY: [0, 0],
+    originPosition: [0, 0], // 初始位置
+    currentPosition: [0, 0], // 当前位置
+    dragging: false, // 拖拽状态
   });
 
+  /**
+   * 开始按下
+   * @param {Event}
+   */
   const handleStart = useCallback(e => {
+    e.stopPropagation();
     const {
-      prevPosition: [prevX, prevY],
-    } = state;
-    let [x, y] = state.prevPosition;
-    if (e instanceof window.TouchEvent) {
+      current: [prevX, prevY], // 获取之前位置
+    } = prevPosition;
+
+    // 声明坐标 之前位置
+    let [x, y] = prevPosition.current;
+    // 是否存在touch事件
+    if (decideEvent(e) === 0) {
+      // 获取x坐标和y坐标
       x = e.touches[0].clientX - prevX;
       y = e.touches[0].clientY - prevY;
-    } else if (e instanceof window.MouseEvent && e.button === 0) {
+      // 是否存在鼠标事件 且 是鼠标左键
+    } else if (decideEvent(e) === 1) {
+      // 获取x坐标和y坐标
       x = e.clientX - prevX;
       y = e.clientY - prevY;
     } else return;
-    setState(_ => ({
-      ..._,
-      dragging: true,
-      startXY: [x, y],
+
+    setState(_state => ({
+      ..._state,
+      dragging: true, // 可以拖动
+      originPosition: [x, y], // 初始位置
     }));
   }, []);
 
+  /**
+   * 开始移动
+   * @param {Event}
+   */
   const handleMove = useCallback(
     e => {
-      if (!state.dragging) return;
+      const {
+        dragging, // 状态
+        originPosition: [startX, startY], // 初始位置
+      } = state;
+      // 非拖动 抛出
+      if (!dragging) return;
       e.preventDefault();
-      const [startX, startY] = state.startXY;
-      let [x, y] = state.prevPosition;
-      if (e instanceof window.TouchEvent) {
+      e.stopPropagation();
+
+      // 声明坐标 之前位置
+      let [x, y] = prevPosition.current;
+      // 是否为移动端
+      if (decideEvent(e) === 0) {
         x = e.touches[0].clientX - startX;
         y = e.touches[0].clientY - startY;
-      } else if (e instanceof window.MouseEvent && e.button === 0) {
+        // 是否PC 且 鼠标左键
+      } else if (decideEvent(e) === 1) {
         x = e.clientX - startX;
         y = e.clientY - startY;
       } else return;
-      setState(_ => ({
-        ..._,
-        prevPosition: [x, y],
-        position: [x, y],
+
+      // 窗口边界
+      const outWindowDistance = e.clientX - (e.clientX - bindRef.current.offsetLeft - (e.clientX - startX));
+      // 最左侧 < 0 抛出
+      if (outWindowDistance < 0) return;
+      // 最右侧 < 0 抛出
+      if (document.body.clientWidth - bindRef.current.offsetWidth - outWindowDistance < 0) return;
+
+      // 更新位置状态
+      prevPosition.current = [x, y];
+
+      setState(_state => ({
+        ..._state,
+        currentPosition: [x, y], // 当前位置
       }));
-      if (ref.current) {
-        ref.current.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+      // 存在Dom
+      if (bindRef.current) {
+        bindRef.current.style.transform = `translate3d(${x}px, ${y}px, 0)`;
       }
     },
     [state.dragging],
   );
 
+  /**
+   * 停止操作
+   * @param {Event}
+   */
   const handleEnd = useCallback(e => {
-    if (e instanceof window.TouchEvent || (e instanceof window.MouseEvent && e.button === 0)) {
-      setState(_ => ({
-        ..._,
-        dragging: false,
+    // 判断是否存在事件
+    if (decideEvent(e) < 2) {
+      setState(_state => ({
+        ..._state,
+        dragging: false, // 停止拖动
       }));
     }
   }, []);
 
   useEffect(() => {
-    const node = ref.current;
+    const node = bindRef.current;
+    // 存在绑定Dom
     if (node) {
+      // 添加事件 移动端
       node.addEventListener('touchstart', handleStart);
       node.addEventListener('touchmove', handleMove, {
         passive: false,
       });
-      document.addEventListener('touchcancel', handleEnd);
       document.addEventListener('touchend', handleEnd);
+      // PC
       node.addEventListener('mousedown', handleStart);
       document.addEventListener('mousemove', handleMove);
       document.addEventListener('mouseup', handleEnd);
     }
+    // 卸载事件
     return () => {
       if (node) {
+        // 移动端
         node.removeEventListener('touchstart', handleStart);
         node.removeEventListener('touchmove', handleMove);
-        document.removeEventListener('touchcancel', handleEnd);
+        document.removeEventListener('touchend', handleEnd);
+        // PC
         node.removeEventListener('mousedown', handleStart);
         document.removeEventListener('mousemove', handleMove);
         document.removeEventListener('mouseup', handleEnd);
       }
     };
-  }, [ref, handleStart, handleMove, handleEnd]);
+  }, [bindRef, handleStart, handleMove, handleEnd]);
 
-  return { target: ref };
+  return { target: bindRef, info: state };
 };
 
 export default useDraggable;
