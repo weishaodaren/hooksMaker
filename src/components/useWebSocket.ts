4ace65f812e8eb235cebbc479e8dc46326e6c39d
weishaodaren <weishaodaren@gmail.com>
2021-04-12 17:55:45 +0800

    update wss

--- src/components/useWebSocket.ts
+++ src/components/useWebSocket.ts
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useCallback, FC } from 'react';
 
 export enum ReadyState {
   Connecting,
@@ -25,3 +25,128 @@ export interface Result {
   readyState: ReadyState;
   webSocketIns?: WebSocket;
 }
+
+export interface IProps {
+  socketUrl: string;
+  options: Options;
+}
+export const useWebSocket: FC<IProps> = (socketUrl, options = {}) => {
+  const {
+    reconnectLimit = 3,
+    reconnectInterval = 3 * 1000,
+    manual = false,
+    onOpen,
+    onClose,
+    onMessage,
+    onError,
+  } = options;
+
+  const reconnectTimesRef = useRef(0);
+  const reconnectTimerRef = useRef<NodeJS.Timeout>();
+  const websocketRef = useRef<WebSocket>();
+
+  const [latestMessage, setLatestMessage] = useState<
+    WebSocketEventMap['message']
+  >();
+  const [readyState, setReadyState] = useState<ReadyState>(ReadyState.Closed);
+
+  /**
+   * 重连
+   */
+  const reconnect = useCallback(() => {
+    if (
+      reconnectTimesRef.current < reconnectLimit &&
+      websocketRef.current?.readyState !== ReadyState.Open
+    ) {
+      reconnectTimerRef.current && clearTimeout(reconnectTimerRef.current);
+
+      reconnectTimerRef.current = setTimeout(() => {
+        connectWs();
+        reconnectTimesRef.current++;
+      }, reconnectInterval);
+    }
+  }, []);
+
+  const connectWs = useCallback(() => {
+    reconnectTimerRef.current && clearTimeout(reconnectTimerRef.current);
+
+    if (websocketRef.current) {
+      websocketRef.current.close();
+    }
+
+    try {
+      websocketRef.current = new WebSocket(socketUrl as any);
+      websocketRef.current.onerror = (event) => {
+        reconnect();
+        onError && onError(event);
+        setReadyState(websocketRef.current?.readyState || ReadyState.Closed);
+      };
+      websocketRef.current.onopen = (event) => {
+        onOpen && onOpen(event);
+        reconnectTimesRef.current = 0;
+        setReadyState(websocketRef.current?.readyState || ReadyState.Closed);
+      };
+      websocketRef.current.onmessage = (
+        message: WebSocketEventMap['message']
+      ) => {
+        onMessage && onMessage(message);
+        setLatestMessage(message);
+      };
+      websocketRef.current.onclose = (event) => {
+        reconnect();
+        onClose && onClose(event);
+        setReadyState(websocketRef.current?.readyState || ReadyState.Closed);
+      };
+    } catch (error) {
+      throw error;
+    }
+  }, []);
+
+  /**
+   * 发送消息
+   * @param message
+   */
+  const sendMessage: WebSocket['send'] = useCallback((message) => {
+    if (readyState === ReadyState.Open) {
+      websocketRef.current?.send(message);
+    } else {
+      throw new Error('WebSocket disconnected');
+    }
+  }, []);
+
+  /**
+   * 手动 connect
+   */
+  const connect = useCallback(() => {
+    reconnectTimesRef.current = 0;
+    connectWs();
+  }, []);
+
+  /**
+   * disconnect websocket
+   */
+  const disconnect = useCallback(() => {
+    reconnectTimerRef.current && clearTimeout(reconnectTimerRef.current);
+
+    reconnectTimesRef.current = reconnectLimit;
+    websocketRef.current?.close();
+  }, []);
+
+  useEffect(() => {
+    // 初始连接
+    if (!manual) {
+      connect();
+    }
+  }, [socketUrl, manual]);
+
+  useEffect(() => () => disconnect(), []);
+
+  return {
+    latestMessage,
+    sendMessage,
+    connect,
+    disconnect,
+    readyState,
+    webSocketIns: websocketRef.current,
+  };
+};
