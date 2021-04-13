0d2edfdd3d0310f126c294d8a8a9c82cfa30fe5f
weishaodaren <weishaodaren@gmail.com>
2021-04-13 10:11:59 +0800

    compare wss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef, FC, useMemo } from 'react';
-import { useLifecycles, useWebSocket } from './components';
+// import { useLifecycles, useWebSocket } from './components';
+import { useWebSocket } from 'ahooks';
 
 interface IProps {
   fn: () => void;
@@ -37,17 +38,20 @@ const App = () => {
     console.log(12);
   };
 
+  const a = Date.now();
   return (
     <div>
       <Btn fn={fn} />
-      <button onClick={() => sendMessage && sendMessage(`${Date.now()}`)}>
-        Send
+      <button onClick={() => sendMessage && sendMessage(`${a}`)}>
+        Send({a})
       </button>
       <button onClick={() => disconnect && disconnect()}>Disconnect</button>
       <button onClick={() => connect && connect()}>Connect</button>
       <div>
         <p>
           received msg:
+          <i>{readyState}</i>
+          <br />
           {messageHistory.current.map((el: { data: any }, index) => {
             <p key={index}>{el?.data}</p>;
           })}
